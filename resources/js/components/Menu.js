7e990a2b5e21287d175b69afc18bda1fa8bc11fe
Daniel Lee <dan.dh.lee@gmail.com>
2019-12-22 23:15:15 -0800

    Initial attempt and top progress bar

--- resources/js/components/Menu.js
+++ resources/js/components/Menu.js
@@ -8,14 +8,17 @@ class Menu extends React.Component {
 
     render() {
         return (
-            <div className="ui secondary menu">
-                <div className="ui container">
-                    <Link to="/" className="item"><strong>Daniel Lee</strong></Link>
-                    <div className="right menu">
-                        <Link to="/about" className="item">About</Link>
-                        <Link to="/projects" className="item">Projects</Link>
-                        <Link to="/contact" className="item">Contact</Link>
-                        <span className="item">dan.dh.lee@gmail.com</span>
+            <div>
+                <div className="menu-progress"></div>
+                <div className="ui secondary menu">
+                    <div className="ui container">
+                        <Link to="/" className="item"><strong>Daniel Lee</strong></Link>
+                        <div className="right menu">
+                            <Link to="/about" className="item">About</Link>
+                            <Link to="/projects" className="item">Projects</Link>
+                            <Link to="/contact" className="item">Contact</Link>
+                            <span className="item">dan.dh.lee@gmail.com</span>
+                        </div>
                     </div>
                 </div>
             </div>
